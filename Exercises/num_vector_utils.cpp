5efd96580244a0082562e7df12c6d4dc60068237
Alex <alex.likes.robots@gmail.com>
2024-11-12 12:41:29 -0500

    Passed my aditional tests

--- Exercises/num_vector_utils.cpp
+++ Exercises/num_vector_utils.cpp
@@ -3,10 +3,14 @@
 using namespace std;
 
 string render_num_vector(const vector<int>& vect) {
-    string s = "";
+    string s = "", ss;
     for (int i = 0; i < vect.size(); i++){
 	s += to_string(vect[i]) + " ";
 }
-    return s;
+    ss = s.substr(0, s.size()-1);
+    return ss;
 }
+
+
+
 
--- Exercises/test_num_vector_utils.cpp
+++ Exercises/test_num_vector_utils.cpp
@@ -9,6 +9,28 @@ using namespace std;
 TEST_CASE("Testing render_num_vector") {
     vector<int> nums = {1, 3, 7};
     string numstr = render_num_vector(nums);
-    string expected = "1 3 7 ";
+    string expected = "1 3 7";
     CHECK(numstr == expected);
 }
+
+
+TEST_CASE("Testing More than 3 ints") {
+    vector<int> nums = {1, 3, 7, 12};
+    string numstr = render_num_vector(nums);
+    string expected = "1 3 7 12";
+    CHECK(numstr == expected);
+}
+
+TEST_CASE("Testing No ints") {
+    vector<int> nums;
+    string numstr = render_num_vector(nums);
+    string expected = "";
+    CHECK(numstr == expected);
+}
+
+
+TEST_CASE("Testing num_vector_sum") {
+    vector<int> nums = {1, 3, 7};
+    CHECK(11 == num_vector_sum(nums));
+}
+
