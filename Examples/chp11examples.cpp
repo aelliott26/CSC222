d7a41c80d85abeff60ceb4eb92bdf432440276d5
Alex <alex.likes.robots@gmail.com>
2024-11-19 19:35:53 -0500

    Finished 5

--- Examples/chp11examples.cpp
+++ Examples/chp11examples.cpp
@@ -7,7 +7,7 @@ struct Time {
 
     void print();
     void increment(double secs);
-
+    double convert_to_seconds() const;
 };
 
 void Time::print()
@@ -29,9 +29,16 @@ void Time::increment(double secs)
     }
 }
 
+double Time::convert_to_seconds() const {
+    return (hour * 60 + minute) * 60 + second;
+}
+
+
 int main() {
     Time current_time = {9, 14, 30.0};
+    current_time.print();
     current_time.increment(500.0);
     current_time.print();
+    cout << current_time.convert_to_seconds() << endl;
     return 0;
 }
