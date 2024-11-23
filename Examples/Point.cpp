87ff86c52a3102f46f7590fad44ffeccd3d641e7
Alex <alex.likes.robots@gmail.com>
2024-11-23 15:37:49 -0500

    Couldn't get hand to work so I moved onto points

---
+++ Examples/Point.cpp
@@ -0,0 +1,21 @@
+#include "Point.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Point::Point(int xx){
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int xx, int yy){
+    x = 0;
+    y = 0;
+}
+
+Point::Point(){
+    x = 0;
+    y = 0;
+}
+
---
+++ Examples/Point.h
@@ -0,0 +1,13 @@
+#ifndef POINT_H
+#define POINT_H
+#include <string>
+using namespace std;
+
+struct Point {
+    int x, y;
+    Point(int xx);
+    Point(int xx, int yy);
+    Point();
+};
+
+#endif
