28c1b57570c04cfed74991c9b0699aa5a3ef562b
Alex <alex.likes.robots@gmail.com>
2024-12-03 09:19:26 -0500

    organized and failed first myobject test

---
+++ Examples/myObject/MyObject.cpp
@@ -0,0 +1,16 @@
+#include "MyObject.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+MyObject::MyObject(){
+    str = "s";
+}
+
+string MyObject::to_string(){
+
+    return "a";
+}
+
+
