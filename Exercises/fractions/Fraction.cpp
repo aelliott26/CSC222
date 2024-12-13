7ba148a7a17ae71f9b61b2a68eb282e49bda1cb9
Alex <alex.likes.robots@gmail.com>
2024-12-12 20:47:12 -0500

    got test 7 to pass

--- Exercises/fractions/Fraction.cpp
+++ Exercises/fractions/Fraction.cpp
@@ -24,8 +24,14 @@ Fraction::Fraction(int i) {
 Fraction::Fraction(string s) {
 	string slash = "/", s2 = s;
 	int slashpos = s.find(slash);
-	numerator = stoi (s.erase(slashpos));
-	denominator = stoi (s2.erase(0,slashpos+1));
+	if (slashpos > 0){
+		numerator = stoi (s.erase(slashpos));
+		denominator = stoi (s2.erase(0,slashpos+1));
+}
+	if (slashpos < 0){
+		numerator = stoi (s);
+		denominator = 1;
+}
 }
 
 string Fraction::to_string(){
